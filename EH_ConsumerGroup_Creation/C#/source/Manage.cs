e75eb47a7c2cfcf9d387d004e5da583ca39144ae
chinmay_kulkarni2 <chinmay_kulkarni2@persistent.co.in>
2014-12-23 10:27:31 +0530

    Checking in code for creating consumergroup on EventHub.

---
+++ EH_ConsumerGroup_Creation/C#/source/Manage.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.ServiceBus.Samples
+{
+    using Microsoft.ServiceBus.Messaging;
+
+    public class Manage
+    {
+
+        public static async Task<EventHubDescription> UpdateEventHub(string eventHubName, string consumerGroupName, NamespaceManager namespaceManager)
+        {
+            // Add a consumer group
+            EventHubDescription ehd = await namespaceManager.GetEventHubAsync(eventHubName);
+            await namespaceManager.CreateConsumerGroupIfNotExistsAsync(ehd.Path, consumerGroupName);
+
+            // Create a customer SAS rule with Manage permissions
+            ehd.UserMetadata = "Some updated info";
+            string ruleName = "myeventhubmanagerule";
+            string ruleKey = SharedAccessAuthorizationRule.GenerateRandomKey();
+            ehd.Authorization.Add(new SharedAccessAuthorizationRule(ruleName, ruleKey, new AccessRights[] { AccessRights.Manage, AccessRights.Listen, AccessRights.Send }));
+
+            EventHubDescription ehdUpdated = await namespaceManager.UpdateEventHubAsync(ehd);
+            return ehd;
+        }
+    }
+}
---
+++ EH_ConsumerGroup_Creation/C#/source/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.ServiceBus.Samples
+{
+    using System.Configuration;
+    using Microsoft.ServiceBus.Messaging;
+
+    class Program
+    {
+        #region Fields
+
+        static string eventHubName;
+        static string consumerGroupName;
+        #endregion
+
+        static void Main(string[] args)
+        {
+            //Initialize
+            ParseArgs(args);
+            string connectionString = GetServiceBusConnectionString();
+            NamespaceManager namespaceManager = NamespaceManager.CreateFromConnectionString(connectionString);
+
+            var e = Manage.UpdateEventHub(eventHubName, consumerGroupName, namespaceManager);
+            var res = e.Result;
+
+
+            Console.WriteLine("Created ConsumerGroup for eventhub. Press any key to stop worker.");
+            Console.ReadLine();
+        }
+
+        static void ParseArgs(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                throw new ArgumentException("Incorrect number of arguments. Expected 2 args <eventhubname> <consumerGroupName>", args.ToString());
+            }
+            else
+            {
+                eventHubName = args[0];
+                Console.WriteLine("EventHub Name: " + eventHubName);
+
+                consumerGroupName = args[1];
+                Console.WriteLine("ConsumerGroup Name: " + consumerGroupName);
+
+            }
+        }
+
+        private static string GetServiceBusConnectionString()
+        {
+            string connectionString = ConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"];
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                Console.WriteLine("Did not find Service Bus connections string in appsettings (app.config)");
+                return string.Empty;
+            }
+            ServiceBusConnectionStringBuilder builder = new ServiceBusConnectionStringBuilder(connectionString);
+            builder.TransportType = TransportType.Amqp;
+            return builder.ToString();
+        }
+    }
+}
